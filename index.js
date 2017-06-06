14fdba9f6b4c5d8f2a31159b3c3f1ac0d045270f
eddicke <adenekanademola931@gmail.com>
2017-06-06 19:14:23 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+var oldMsg = []
+io.on('connection', function(socket) {
+  io.emit("oldMsg", oldMsg);
+  socket.on('chat', function(username, message) {
+    console.log('message received, sent by: ' + username + ', content: ' + message);
+    oldMsg.push('message received, sent by: ' + username + ', content: ' + message);
+    io.emit('chat', username, message);
+  });
+ socket.on('ademola', function(gme){
+    io.emit('ademola', gme);
+  });
+ socket.on('hunts', function(me){
+    io.emit('hunts', me);
+  });
+
+});
+
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
